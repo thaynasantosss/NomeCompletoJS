37f75592ff17edec1ebde75d9563694b255aa909
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2023-11-13 19:14:39 -0300

    tarefa realizada

---
+++ script.js
@@ -0,0 +1,5 @@
+let nome = prompt('Digite seu primeiro nome: ')
+        let sobrenome = prompt('Digite seu sobrenome: ')
+        let nomeCompleto = `${nome} ${sobrenome}`
+        alert(`Nome completo: ${nomeCompleto}`)
+        alert(`Nome de catálogo: ${sobrenome.toUpperCase()}, ${nome}.`)
